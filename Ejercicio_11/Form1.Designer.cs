836d01c66eb221a5499f97af2a69fdeed6958a00
LRx28 <elluisjose2@gmail.com>
2026-02-09 00:28:37 -0400

    Ultima modificacion ejercicio 11

--- Ejercicio_11/Form1.Designer.cs
+++ Ejercicio_11/Form1.Designer.cs
@@ -77,6 +77,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveCaption;
             ClientSize = new Size(800, 450);
             Controls.Add(label1);
             Controls.Add(lbl_FechaHora);
